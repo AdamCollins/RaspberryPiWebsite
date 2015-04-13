0d0f4da7ed0207168054ab6d3717fcc3f5e4bd9b
Adam Collins <adamcollinssecure.com>
2015-04-12 21:54:43 -0700

    First Commit

---
+++ myScript.js
@@ -0,0 +1,17 @@
+function homePg(){
+
+		alert("it works")
+}
+
+function currentpage(){
+					if(page=)
+						alert("You are curently on " + page);
+							}
+
+
+
+
+
+function image(){
+				   alert("SD card not included");
+						}
